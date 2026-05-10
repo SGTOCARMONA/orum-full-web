57731af5ee1412f7ad7fbe1f1d0c188493909705
SGTOCARMONA <juancarmonalopez89@gmail.com>
2026-05-10 18:23:02 +0200

    Initial commit - Versión limpia para despliegue

---
+++ dist/server/assets/_tanstack-start-manifest_v-C5NrIk7S.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/Juanillo/Desktop/Trasteando/Web Lovable/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-Bapkmm1q.js"] }, "/": { filePath: "C:/Users/Juanillo/Desktop/Trasteando/Web Lovable/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-4JgOm-X6.js"] } }, clientEntry: "/assets/index-Bapkmm1q.js" });
+export {
+  tsrStartManifest
+};
---
+++ dist/server/index.js
@@ -0,0 +1,5 @@
+import { w } from "./assets/worker-entry-C9BoQXpX.js";
+import "node:events";
+export {
+  w as default
+};
